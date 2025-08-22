0faca5dea6767454019b0ee276d8b45d181ad495
rudeboyjamesp <rudeboyjamesp@gmail.com>
2025-08-23 00:42:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
