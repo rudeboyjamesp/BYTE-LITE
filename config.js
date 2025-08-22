7e425dc2362f3cab3b70178147a4ef77d1ff3d75
rudeboyjamesp <rudeboyjamesp@gmail.com>
2025-08-22 23:24:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "Byte;;;eyJub2lzZUtleSI6e yJwcml2YXRlljp7InR5cGU iOiJCdWZmZXIiLCJKYXR hljoiNERWOVJ3dmUvTH dyRkdJSnRRYkdPNUtmY...",
     CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴅᴇʀᴇᴅ ʙʏ ᴛᴀʟᴋᴅʀᴏᴠᴇ*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
